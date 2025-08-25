e3600265f536c4fed8e50db074be23be279f6bbc
Derek Wisher <inluawisher@gmail.com>
2025-08-25 13:43:17 -0700

    Padding and Alignment

--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@ decimal taxPercentage = .15825m;
 decimal total = 3185.19m;
 
 Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3}");
+Console.WriteLine($"   Subtotal: {subtotal:C}");
+Console.WriteLine($"   Tax: {taxPercentage:P2}");
+Console.WriteLine($"   Total: {total:C}");
