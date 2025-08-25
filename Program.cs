887805e81084543727d1b9f4772f47f6b94c4bba
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-25 08:00:21 -0700

    Setup

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
 ﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string[] pangramWords = pangram.Split(" ");
+string[] reversedWords = new string[pangram.Length];
+string reversedWordsPangram = "";
+
+foreach (string word in pangramWords)
+{
+    char[] chars = word.ToCharArray();
+    Console.WriteLine(chars);
+}
