23659da06512cab73b194e80675d730eaf18add7
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-25 08:59:51 -0700

    Interpolation Formatting

--- Program.cs
+++ Program.cs
@@ -3,4 +3,5 @@ decimal salePrice = 59.99m;
 
 string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
 
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
 Console.WriteLine(yourDiscount);
