14657593b825aa119325fe7fd5b9192ea3fd9388
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-25 08:20:10 -0700

    First and Possibly Final Test

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderArray = orderStream.Split(',');
+Array.Sort(orderArray);
+
+for (int i = 0; i < orderArray.Length; i++)
+{
+    if (orderArray[i].Length != 3)
+    {
+        orderArray[i] = orderArray[i] + "\t - Error";
+    }
+    Console.WriteLine(orderArray[i]);
+}
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a0f98071d9d5d48e4a4599b017cf17920a752d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41d987a09cd245cf40a8fc818b0fffe27733d24c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
