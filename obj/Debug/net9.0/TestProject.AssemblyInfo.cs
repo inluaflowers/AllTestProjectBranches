3d8405ed5d3224c11135a1a19c01a769951e65e7
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-24 18:48:41 -0700

    Accessing Null Indexes

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699a6b90dfda4d11acf31544e4bc7150bc448550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d0b0d99baee79829a94f8996477333fa4f4467")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
