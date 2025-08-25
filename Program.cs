535e5834a17ade60d30d9676e26887224ad37317
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-25 08:33:34 -0700

    InterpolationAndCurrency

--- Program.cs
+++ Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14657593b825aa119325fe7fd5b9192ea3fd9388")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff00e5083809c1e7e37477ee64e510deca18fe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
