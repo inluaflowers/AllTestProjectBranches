3ff00e5083809c1e7e37477ee64e510deca18fe7
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-25 08:29:28 -0700

    Initial

--- Program.cs
+++ Program.cs
@@ -1,12 +1,1 @@
-﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-string[] orderArray = orderStream.Split(',');
-Array.Sort(orderArray);
-
-for (int i = 0; i < orderArray.Length; i++)
-{
-    if (orderArray[i].Length != 4)
-    {
-        orderArray[i] = orderArray[i] + "\t - Error";
-    }
-    Console.WriteLine(orderArray[i]);
-}
+﻿
