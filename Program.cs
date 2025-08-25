41d987a09cd245cf40a8fc818b0fffe27733d24c
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-25 08:15:56 -0700

    StartNewChallenge

--- Program.cs
+++ Program.cs
@@ -1,16 +1,1 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
-string[] pangramWords = pangram.Split(" ");
-string[] reversedWords = new string[pangram.Length];
-string reversedWordsPangram;
-
-for (int i = 0; i < pangramWords.Length; i++)
-{
-    char[] chars = pangramWords[i].ToCharArray();
-    Array.Reverse(chars);
-    string newWord = String.Join("", chars);
-    Console.WriteLine(newWord);
-    reversedWords[i] = newWord;
-}
-
-reversedWordsPangram = String.Join(" ", reversedWords);
-Console.WriteLine(reversedWordsPangram);
+﻿
