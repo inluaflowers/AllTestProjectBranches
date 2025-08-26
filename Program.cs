d5f6b2d6b896215b72f8d701b536c49a1baa146d
Derek Wisher <inluawisher@gmail.com>
2025-08-26 06:30:15 -0700

    ConstNotMagic

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿string message = "Find what is (inside the parentheses)";
+﻿string message = "What is the value <span>between the tags</span>?";
 
-int openingPosition = message.IndexOf('(');
-int closingPosition = message.IndexOf(')');
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
 
-Console.WriteLine(openingPosition);
-Console.WriteLine(closingPosition);
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d59fdf551145092be52dbd27e9f7bd47ec67a584")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff092ab1748b44c8c5eff7b9c11ffb118e5cffbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
