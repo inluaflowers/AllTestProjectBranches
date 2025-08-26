ec2bf880ea4f08e702345dde853f6e1901a4ab76
Derek Wisher <inluawisher@gmail.com>
2025-08-25 18:50:37 -0700

    Before Test

--- Program.cs
+++ Program.cs
@@ -3,6 +3,23 @@ string second = "World";
 string result = string.Format("{0} {1}!", first, second);
 Console.WriteLine(result); */
 
-string input = "Pad this";
+/* string input = "Pad this";
  Console.WriteLine(input.PadLeft(12, '-'));
- Console.WriteLine(input.PadRight(12, '-'));
+ Console.WriteLine(input.PadRight(12, '-')); */
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+
+string test1 = "12345";
+string test2 = "567788";
+
+Console.WriteLine(test1.PadLeft(10));
+Console.WriteLine(test2.PadLeft(10));
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca263ecf048c4ac8c8403eb00992e3a1d5db0d95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a885c38896a80567245164f102b8f3e601885a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
