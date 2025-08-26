4a885c38896a80567245164f102b8f3e601885a9
Derek Wisher <inluawisher@gmail.com>
2025-08-25 18:23:58 -0700

    PaddingOverloaded

--- Program.cs
+++ Program.cs
@@ -4,4 +4,5 @@ string result = string.Format("{0} {1}!", first, second);
 Console.WriteLine(result); */
 
 string input = "Pad this";
- Console.WriteLine(input.PadLeft(12));
+ Console.WriteLine(input.PadLeft(12, '-'));
+ Console.WriteLine(input.PadRight(12, '-'));
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3600265f536c4fed8e50db074be23be279f6bbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca263ecf048c4ac8c8403eb00992e3a1d5db0d95")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
