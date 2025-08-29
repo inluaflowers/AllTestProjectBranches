1120488f7f73c65efd104386c790ed7b106d74ee
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-28 20:28:31 -0700

    Challenge Complete

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
 ﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+int openSpanStart = input.IndexOf(openSpan);
+int openSpanEnd = openSpanStart + openSpan.Length;
+int closeSpanStart = input.IndexOf(closeSpan);
+int subStringLength = closeSpanStart - openSpanEnd;
 
-string quantity = "";
-string output = "";
+// value between <span> and </span>
+string quantity = input.Substring(openSpanEnd, subStringLength);
+// set to value of input, then remove <div> and </div>
+string output = input;
+output = output.Replace("<div>", "");
+output = output.Replace("</div>", "");
+output = output.Replace("&trade;", "&reg;");
 
-// Your work here
+// Replace (&trade;) with (&reg;) (HTML Characters)
 
-Console.WriteLine(quantity);
-Console.WriteLine(output);
+
+// Should look like:
+// Quantity: 5000
+// Output: <h2>Widgets &reg;</h2><span>5000</span>
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
