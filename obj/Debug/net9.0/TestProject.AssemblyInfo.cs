337415d1af6cfddd28285e9ba58c2f0fadf1f059
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-30 06:54:57 -0700

    Methods with Parameters

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91051b268255ff9c437ee7b03b2360f714aa23c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8799f749bbdbdee8654b2ce344e6c337f4bfd4bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
