c60b6e13b7a8ee68b86679c0da003efd639b99ac
[Derek Wisher] <[inluawisher@gmail.com]>
2025-08-29 18:56:40 -0700

    Start Guided Animal Shelter Project

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb56d52530659799ec2bebad909b226e3ebded16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1120488f7f73c65efd104386c790ed7b106d74ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
